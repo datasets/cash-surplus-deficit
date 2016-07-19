947d0ccee3b133951d09828b59797e0dd72b1e36
gsilvapt <gsilvapt>
2016-07-19 16:14:48 +0100

    [scripts] overall improvements

--- scripts/process.py
+++ scripts/process.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
-
+import csv, os, sys
 import pandas as pd
+import numpy as np
 
 apiBase = "http://api.worldbank.org/indicator/"
 apiIndicator = "GC.BAL.CASH.GD.ZS"    # This can be changed to any other indicator
@@ -8,11 +9,22 @@ FILE_NAME = 'gini-index.csv'
 source = apiBase+apiIndicator+"?format=csv"
 
 
-def main ():
+def main():
     data=pd.read_csv(source)
-    data.to_csv('data/cash-surp-def.csv', sep=",", index_col=0, index=False)
+    data.to_csv('archive/cash-surp-def.csv', sep=",", index_col=0, index=False)
+    print ('Successly retrieve data.')
 
-print('Success!')
+    
+    # Python is printing "Country Name", thus getting some erros in the end
+    df = pd.read_csv("archive/cash-surp-def.csv")
+    df.columns.values[0] = 'Country Name'
+    
+    df = pd.melt(df, id_vars=['Country Name', 'Country Code'], var_name="Year", value_name="Value")
+    df = df.sort_values(by=['Country Name', 'Year'], ascending=[True, True])
+    
+    df.dropna().to_csv('data/cash-surp-def.csv', sep=",", index=False)
+    
+    print ("File Saved.")
 
 if __name__ == '__main__':
     main()
