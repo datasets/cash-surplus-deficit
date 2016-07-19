7f976956bad97dc04334e9d8e5b641a8272089fb
gsilvapt <gsilvapt>
2016-07-19 15:57:33 +0100

    [scripts] generic improvements

--- scripts/process.py
+++ scripts/process.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
+
 import pandas as pd
 
 apiBase = "http://api.worldbank.org/indicator/"
-apiIndicator = "SI.POV.GINI"    # This can be changed to any other indicator
+apiIndicator = "GC.BAL.CASH.GD.ZS"    # This can be changed to any other indicator
 FILE_NAME = 'gini-index.csv'
 source = apiBase+apiIndicator+"?format=csv"
 
@@ -9,3 +11,8 @@ source = apiBase+apiIndicator+"?format=csv"
 def main ():
     data=pd.read_csv(source)
     data.to_csv('data/cash-surp-def.csv', sep=",", index_col=0, index=False)
+
+print('Success!')
+
+if __name__ == '__main__':
+    main()
