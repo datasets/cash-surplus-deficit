8a81abfcb1c670a6e6f198878cfe081d7241af45
gsilvapt <gsilvapt>
2016-07-19 17:35:24 +0100

    [misc] failing build due to special characters in the data

--- scripts/process.py
+++ scripts/process.py
@@ -17,11 +17,16 @@ def main():
     
     # Python is printing "Country Name", thus getting some erros in the end
     df = pd.read_csv("archive/cash-surp-def.csv")
+    
+    # Data comes with several special characters and spaces
     df.columns.values[0] = 'Country Name'
     
     df = pd.melt(df, id_vars=['Country Name', 'Country Code'], var_name="Year", value_name="Value")
     df = df.sort_values(by=['Country Name', 'Year'], ascending=[True, True])
     
+    df['Country Name'] = df['Country Name'].replace(' |\.|"|,|\'|:|-|\(|\)', '', regex=True)
+    df['Country Name'] = df['Country Name'].replace('&', 'AND', regex=True)
+
     df.dropna().to_csv('data/cash-surp-def.csv', sep=",", index=False)
     
     print ("File Saved.")
