40bdea2d6167b89add0a78da5794418868135374
gradedSystem <homesiknessgerald@gmail.com>
2024-12-17 18:16:55 +0800

    [update][m] Updating the scripts

--- scripts/process.py
+++ scripts/process.py
@@ -1,35 +1,110 @@
 #!/usr/bin/python
-import csv, os, sys
-import pandas as pd
-import numpy as np
+import os
+import io
+import csv
+import json
+import shutil
+import zipfile
+import requests
 
-apiBase = "http://api.worldbank.org/indicator/"
-apiIndicator = "GC.BAL.CASH.GD.ZS"    # This can be changed to any other indicator
+from datetime import datetime
+
 FILE_NAME = 'cash-suprlus-deficit.csv'
-source = apiBase+apiIndicator+"?format=csv"
+source = "https://api.worldbank.org/v2/en/indicator/GC.BAL.CASH.GD.ZS?downloadformat=csv"
+script_dir = os.path.dirname(os.path.abspath(__file__))
+cache = 'cache'
+datapackage = 'datapackage.json'
+current_year = datetime.now().year
+outheadings = ['Country Name', 'Country Code', 'Year', 'Value']
+data = 'data/cash-surp-def.csv'
+archive = "archive/cash-surp-def.csv"
+def search_files_in_cache():
+    cache_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cache')
 
+    if not os.path.exists(cache_dir):
+        print("Cache folder does not exist!")
+        return None
 
-def main():
-    data=pd.read_csv(source)
-    data.to_csv('archive/cash-surp-def.csv', sep=",", index_col=0, index=False)
-    print ('Successly retrieve data.')
+    filtered_files = [f for f in os.listdir(cache_dir) if 'metadata' not in f.lower()]
 
+    if not filtered_files:
+        print("No valid files found in cache.")
+        return None
     
-    # Python is printing "Country Name", thus getting some erros in the end
-    df = pd.read_csv("archive/cash-surp-def.csv")
-    
-    # Data comes with several special characters and spaces
-    df.columns.values[0] = 'Country Name'
-    
-    df = pd.melt(df, id_vars=['Country Name', 'Country Code'], var_name="Year", value_name="Value")
-    df = df.sort_values(by=['Country Name', 'Year'], ascending=[True, True])
-    
-    df['Country Name'] = df['Country Name'].replace('\.|"|,|\'|:|-|\(|\)', '', regex=True)
-    df['Country Name'] = df['Country Name'].replace('&', 'AND', regex=True)
+    # Update data in archive folder
+    with open('scripts/cache/'+filtered_files[0],'r') as firstfile, open(archive,'a') as secondfile: 
+        for line in firstfile: 
+            secondfile.write(line)
 
-    df.dropna().to_csv('data/cash-surp-def.csv', sep=",", index=False)
-    
-    print ("File Saved.")
+    return filtered_files
 
+def transform_csv(dest):
+    with open(dest, 'r') as f:
+        reader = csv.reader(f)
+        next(reader)
+        next(reader)
+        updated_date_row = next(reader)
+        last_updated_date = updated_date_row[1].split("-")
+        last_updated_date = f"{last_updated_date[0]}-{last_updated_date[1]}-{last_updated_date[2]}"
+
+        next(reader) 
+        header = next(reader)
+        transformed_data = []
+        for row in reader:
+            country_name = row[0]  
+            country_code = row[1]  
+
+            for i in range(4, len(row)):
+                year = header[i]
+                value = row[i]
+
+                if value:
+                    transformed_data.append([country_name, country_code, year, value])
+    
+    return last_updated_date, transformed_data
+
+def update_datapackage(last_updated):
+    with open(datapackage, 'r') as f:
+        dp = json.load(f)
+
+    dp['last_updated'] = str(last_updated)
+    dp['version'] = str(current_year)
+
+    with open(datapackage, 'w') as f:
+        json.dump(dp, f, indent=2)
+
+def extract_zip():
+    cache_dir = os.path.join(script_dir, cache)
+
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+
+    try:
+        r = requests.get(source, stream=True)
+        r.raise_for_status()
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall(path=cache_dir)
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading the file: {e}")
+        return
+
+def process():
+    extract_zip()  # Ensure ZIP extraction happens first
+    file_name = search_files_in_cache()[0]
+    dest = os.path.join(script_dir, cache, file_name)
+    last_updated, transformed_data = transform_csv(dest)
+
+    output_dir = os.path.dirname('data/')
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    with open(os.path.abspath(data), 'w', newline='') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(outheadings)
+        writer.writerows(transformed_data)
+
+    update_datapackage(last_updated)
+    shutil.rmtree(os.path.join(script_dir, cache))
+    
 if __name__ == '__main__':
-    main()
+    process()
